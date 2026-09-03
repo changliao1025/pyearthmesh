0daabd59e05364aa6ce4aae5684192a0486536c3
Chang Liao <changliao1025@outlook.com>
2026-09-03 22:31:41 +0800

    updatee viz test

--- examples/latlon/example_create_lanlon.py
+++ examples/latlon/example_create_lanlon.py
@@ -12,7 +12,7 @@ dLatitude_top_in=37.5
 dResolution_degree_in=0.50
 ncolumn_in = 4
 nrow_in = 4
-sFilename_output_in= r'C:\\Users\\chang\\workspace\\python\\pyearthmesh\\data\\qinghaihu\\lake_latlon.geojson'
+sFilename_output_in= r'C:\\Users\\chang_rnrigc1\\workspace\\python\\pyearthmesh\\data\\qinghaihu\\lake_latlon.geojson'
 pBoundary_in = None
 create_latlon_mesh(dLongitude_left_in, dLatitude_bot_in, 
 dResolution_degree_in, ncolumn_in, nrow_in, sFilename_output_in,
